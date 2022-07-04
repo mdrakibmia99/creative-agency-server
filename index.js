18fb48caa40b83ba6f24098d9b012a6ff7b68282
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-04 11:32:01 +0600

    add database file

--- index.js
+++ index.js
@@ -16,13 +16,24 @@ app.use(bodyParser.json());
 
 
 
-const uri = "mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.5yugt.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.5yugt.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
 
+async function run() {
+    try {
+        await client.connect();
 
+        // const collection = database.db('test').collection('devices');
+        // console.log('Creative agency server successfully connected!');
+    } finally {
+        // await database.close();
+    }
+} run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
