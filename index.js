b31e1afb781c67877a30a907fe9160e8356476d3
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-04 11:09:47 +0600

    add middle ware and body parse

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const bodyParser = require('body-parser');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+// connect middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// enable requests
 app.get('/', (req, res) => {
     res.send(`<h1>Creative agency server Connected.</h1>`);
 })
