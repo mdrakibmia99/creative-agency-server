31c81cd213231a4fcf2d22134c7a41e729ded4ac
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-04 11:27:16 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 // required middleware
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 var cors = require('cors')
 const bodyParser = require('body-parser');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+// user name: mdrakib
+// user pass:38be0sBSfgDj9Mub
+
 // connect middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+
+const uri = "mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.5yugt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    serverApi: ServerApiVersion.v1 });
+
+
+
 
 app.get('/', (req, res) => {
     res.send(`<h1>Creative agency server Connected.</h1>`);
@@ -17,3 +32,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Creative agency server listening on port ${port}`);
 })
+
+
+
